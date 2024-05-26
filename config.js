d39a16fadf2e0dbd57ded5adf5075ad2c1800cee
ankit860 <47587806+ankit860@users.noreply.github.com>
2024-05-26 17:28:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -48,11 +48,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘-༒ＤEVIL༒ANSH🕸️",
-  author: process.env.PACK_AUTHER || "🕷️༒ＤEVIL༒ANSH༒🕸️",
+  caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘-ＤEVIL༒ANSH",
+  author: process.env.PACK_AUTHER || "༒ＤEVIL༒ANSH༒",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "🕷️༒ＤEVIL༒ANSH༒🕸️",
-  ownername: process.env.OWNER_NAME || "🕷️༒ＤEVIL༒ANSH༒🕸️",
+  botname: process.env.BOT_NAME || "༒ＤEVIL༒ANSH༒",
+  ownername: process.env.OWNER_NAME || "༒ＤEVIL༒ANSH༒",
   errorChat: process.env.ERROR_CHAT || "917376279802",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +64,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  LANG: (process.env.THEME || "🕸️༒ＤEVIL༒ANSH༒🕸️").toUpperCase(),
+  LANG: (process.env.THEME || "༒ＤEVIL༒ANSH༒").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
